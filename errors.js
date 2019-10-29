8f1c8b732995561bba42b563c51c7ce220c78852
Martin Acosta <tinchoz49@gmail.com>
2019-10-29 13:25:41 -0300

    Updated nanoerror.

--- errors.js
+++ errors.js
@@ -1,24 +1,9 @@
-const format = require('quick-format-unescaped')
+const nanoerror = require('nanoerror')
 
 const errors = {}
 
 function createError (code, message) {
-  errors[code] = class extends Error {
-    constructor (...args) {
-      super(format(message, args))
-
-      this.name = code
-
-      if (typeof Error.captureStackTrace === 'function') {
-        Error.captureStackTrace(this, this.constructor)
-      } else {
-        this.stack = (new Error(this.message)).stack
-      }
-
-      this.code = code
-      this.args = args
-    }
-  }
+  errors[code] = nanoerror(code, message)
 }
 
 createError('NCTX_ERR_INVALID_CONTEXT_ARGUMENT', 'Context argument must be a valid object: %s.')
